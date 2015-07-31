9cce7f055d9384b989ec8e3feb37b0299ebe00f3
mezbahalam <mezbahalam26@gmail.com>
2015-07-31 07:58:09 +0600

    sinatra created

---
+++ app.rb
@@ -0,0 +1,12 @@
+class App < Sinatra::Base
+  get "/" do
+    @images = Image.all
+    haml :index
+  end
+
+  post "/images" do
+   @image = Image.new params[:image]
+   @image.save
+   redirect "/"
+  end
+end
