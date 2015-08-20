e69fe459a7df5484f80cc6e81ceba80692d8ac8e
mezbahalam <mezbahalam26@gmail.com>
2015-08-20 18:20:03 +0600

    add sharethis

--- app.rb
+++ app.rb
@@ -4,9 +4,29 @@ class App < Sinatra::Base
     haml :index
   end
 
+  get "/images/:id" do
+    @image = Image[params[:id]]
+    haml :show
+  end
+
   post "/images" do
+   protected!
    @image = Image.new params[:image]
    @image.save
    redirect "/"
+  end
+
+  helpers do
+    def protected!
+      return if authorized?
+      headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+      halt 401, "Not authorized\n"
+    end
+
+    def authorized?
+      @auth ||= Rack::Auth::Basic::Request.new(request.env)
+      @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['mezbah', 'password']
+    end
+
   end
 end
